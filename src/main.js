9bcdea9baef83b161885003da769ce24a00b75c6
jasonding <djhdder@aliyun.com>
2019-08-31 10:49:13 +0800

    set up dev environment

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//================= 第三方库 =================================================
+//Mint UI 
+import MintUI from "mint-ui"
+import "mint-ui/lib/style.css"
+Vue.use(MintUI);
+
+//Axios
+import axios from "axios"
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://127.0.0.1:8080/";
+Vue.prototype.axios = axios;
+
 Vue.config.productionTip = false
 
 new Vue({
