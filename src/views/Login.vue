d54eea283c136a67af7b7eb650ecad01a248a103
jasonding <djhdder@aliyun.com>
2019-08-31 17:52:37 +0800

    edited login logic

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,8 +8,9 @@
         @focus.native.capture="showinfo(1)"
         @blur.native.capture="showinfo(0)"
         v-model="uname"
+        :class="[errorN==1?'errorStyle':'']"
       ></mt-field>
-      <span class="msgStyle" :class="[isActive==1?'showInfo':'']">6~20位字母与数字</span>
+      <span class="msgStyle" :class="[isActive==1?'showInfo':'']">合法的 邮箱地址 或 手机号码</span>
       <mt-field
         class="inputStyle"
         type="password"
@@ -17,6 +18,7 @@
         @focus.native.capture="showinfo(2)"
         @blur.native.capture="showinfo(0)"
         v-model="upwd"
+        :class="[errorP==2?'errorStyle':'']"
       ></mt-field>
       <span class="msgStyle" :class="[isActive==2?'showInfo':'']">6~12位字母与数字</span>
       <mt-button class="loginBtn" plain type="primary" @click="login">登录</mt-button>
@@ -28,7 +30,9 @@ export default {
   data() {
     return {
       isActive: 0,
-      active: 0,
+      actived: 0,
+      errorN: 0,
+      errorP: 0,
       uname: "",
       upwd: ""
     };
@@ -38,7 +42,7 @@ export default {
       this.$toast("希望成功");
     },
     login() {
-      //判断用户名密码格式
+      //判断用户名密码是否为空
       let uname = this.uname;
       let upwd = this.upwd;
 
@@ -58,7 +62,8 @@ export default {
       }
     },
     showinfo(n) {
-      let regName = /^[0-9a-z]{6,20}$/i;
+      let regName = /^^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/i;
+      let regName2 = /^1[3456789]\d{9}$/;
       let regPwd = /^[0-9a-z]{6,12}$/i;
       this.isActive = n;
       if (n == 1) {
@@ -66,16 +71,20 @@ export default {
       } else if (n == 2) {
         this.actived = 2;
       }
-      console.log(this.isActive, this.actived);
+      // console.log(this.isActive, this.actived);
+      //判断用户名密码格式
       if (n == 0) {
         switch (this.actived) {
           case 1:
-            if (this.uname !== "" && !regName.test(this.uname)) {
+            if (this.uname !== "" && !(regName.test(this.uname)||regName2.test(this.uname))) {
               this.$toast({
                 message: "用户名格式不正确",
                 position: "bottom"
               });
+              this.errorN = 1;
               return;
+            } else if (this.uname !== "") {
+              this.errorN = 0;
             }
           case 2:
             if (this.upwd !== "" && !regPwd.test(this.upwd)) {
@@ -83,7 +92,10 @@ export default {
                 message: "密码格式不正确",
                 position: "bottom"
               });
+              this.errorP = 2;
               return;
+            } else if (this.upwd !== "") {
+              this.errorP = 0;
             }
         }
       }
@@ -127,5 +139,8 @@ export default {
 /* 动态添加的样式 */
 .showInfo {
   opacity: 1 !important;
+}
+.errorStyle {
+  border-bottom: 2px solid #ff0000b0 !important;
 }
 </style>
