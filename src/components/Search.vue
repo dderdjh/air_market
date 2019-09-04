4503f20966ffece5a69dd5f7e0de7db9c2f7000d
jasonding <djhdder@aliyun.com>
2019-09-04 21:00:54 +0800

    work on searchbar

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <div class="searchBar">
-      <input class="searchInput" type="text" placeholder="搜索">
+      <input class="searchInput" type="text" placeholder="搜索" v-model="value">
+      <img src="../assets/cancel.png" class="cancelBtn" @click="cancel" v-if="isShow">
       <img src="../assets/search.png" >
     </div>
   </div>
@@ -11,8 +12,24 @@ export default {
   data() {
     return {
       value:"",
+      isShow:false,
     }
   },
+  methods: {
+    cancel(){
+      this.value="";
+    }
+  },
+  watch:{
+    value(val){
+      console.log(val)
+      if(val.length>0){
+        this.isShow = true;
+      }else{
+        this.isShow = false;
+      }
+    }
+  }
 }
 </script>
 
@@ -21,13 +38,21 @@ export default {
     display: flex;
     justify-content: space-between;
     padding: 10px;
+    background-color: #12b1f01e;
+    position: relative;
   }
   .searchInput{
     width: 90%;
-    padding-left: 5px;
+    padding-left: 20px;
+    padding-right: 42px;
     outline: 0;
-    border-top:0; 
-    border-radius: 0; 
-    
+    border:0; 
+    border-radius: 16px;
+  }
+  .cancelBtn{
+    position: absolute;
+    top:50%;
+    right:57px;
+    margin-top:-8px;
   }
 </style>
