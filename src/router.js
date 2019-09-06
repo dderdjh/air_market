fbf234840f44e9abcf135c097556b34e31b24ca0
Jason Ding <djhdder@aliyun.com>
2019-09-06 23:28:04 +0800

    change router

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ import Reg from "./views/Reg.vue"
 import Index from "./views/Index.vue"
 
 ///==========组件测试
-import Search from "./components/Search.vue"
+import AirTypes from "./components/AirTypes.vue"
 
 
 Vue.use(Router)
@@ -31,9 +31,9 @@ export default new Router({
       component: Index
     },
     {
-      path: "/Search",
-      name: "search",
-      component: Search
+      path: "/AirTypes",
+      name: "airtypes",
+      component: AirTypes
     }
   ]
 })
