695f2c7656fb9ce7ca567698d8c442135fda16b8
jasonding <djhdder@aliyun.com>
2019-09-07 14:34:14 +0800

    add avatar

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,13 +1,7 @@
 <template>
   <div>
     <airtype></airtype>
-    <ul
-      v-infinite-scroll="loadMore"
-      infinite-scroll-disabled="loading"
-      infinite-scroll-distance="10"
-    >
-      <li v-for="item in list">{{ item }}</li>
-    </ul>
+    
   </div>
 </template>
 <script>
@@ -16,23 +10,15 @@ import AirType from "./AirType.vue";
 export default {
   data(){
     return{
-      list:["a",2,3,4,5]
+      
     }
   },
   components: {
     airtype: AirType
   },
   methods: {
-    loadMore() {
-      this.loading = true;
-      setTimeout(() => {
-        let last = this.list[this.list.length - 1];
-        for (let i = 1; i <= 10; i++) {
-          this.list.push(last + i);
-        }
-        this.loading = false;
-      }, 2500);
-    }
+    
+    
   }
 };
 </script>
--- src/components/MePage.vue
+++ src/components/MePage.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    <h1>me</h1>
+  <div class="pageWrap">
+    <div class="avatar">
+      <img src="../assets/avatar/i6.png" >
+    </div>
   </div>
 </template>
+<style scoped>
+  .avatar{
+    width: 100px;
+    height: 100px;
+    background-color: #12b1f0a6;
+    border-radius: 50%;
+    padding: 10px;
+    position: relative;
+    
+  }
+  .avatar img{
+    width: 80px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-top:-40px;
+    margin-left:-40px;
+
+  }
+</style>
