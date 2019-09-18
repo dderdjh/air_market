f193b99bc7e53dcdd9373b94018d592705f2532a
jasonding <djhdder@aliyun.com>
2019-09-18 13:43:28 +0800

    add porductloader component

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,49 +1,43 @@
 <template>
   <div>
     <airtype></airtype>
-    <div class="goods" v-for="(item,index) of list" :key="index">
-      <!-- 商品图片 -->
-      <img :src="'http://127.0.0.1:8080/'+item.img_url" >
-      <!-- 商品名称 -->
-      <h5>{{item.title}}</h5>
-      <h6>{{item.subtitle}}</h6>
-      <div>{{item.price}}</div>
-    </div>
+    <porductloader></porductloader>
   </div>
 </template>
 <script>
 // 引入组件
 import AirType from "./AirType.vue";
+import PorductLoader from "./PorductLoader.vue";
 export default {
   data(){
     return{
-      list:[],
-      pno:0
+      
     }
   },
-  created(){
-    this.loadMore();
-  },
+  created(){},
   components: {
-    airtype: AirType
+    airtype: AirType,
+    porductloader: PorductLoader
   },
   methods: {
-    loadMore(){
-      //功能:当组件创建成功后获取第一页数据
-      // 1.创建一个url地址
-      var url="gas";
-      //1.1.将当前页码加一
-      this.pno++;
-      var obj = {pno:this.pno};
-      // 2.发送afax请求获取第一页数据
-      this.axios.get(url,{params:obj}).then(res=>{
-      // 3.将数据保存在data中
-        // console.log(res.data.data);
-        // this.list.concat() = res.data.data;
-        var rows = this.list.concat(res.data.data); //页的拼接
-        this.list = rows;
-      });
-    }
+    
   }
 };
 </script>
+<style scoped>
+  .goods{
+    width:40%;
+    box-sizing: border-box;
+    height:auto;
+    display: flex;
+    flex-direction: column;
+    margin:10px;
+    border: 1px solid #00aaef;
+    border-radius: 5px;
+    padding: 5px;
+  }
+  .goods-img{
+    width:100%;
+    border-radius: 5px;
+  }
+</style>
---
+++ src/components/PorductLoader.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="goodsWrap">
+    <div class="goods" v-for="(item,index) of list" :key="index">
+      <!-- 商品图片 -->
+      <img class="goods-img" :src="'http://127.0.0.1:8080/'+item.img_url" >
+      <!-- 商品名称 -->
+      <div class="goods-info">
+        <h4 class="goods-title">{{item.title}}</h4>
+        <h5 class="goods-subtitle">{{item.subtitle}}</h5>
+        <div class="goods-price">消耗: {{item.price}} 兑换量</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      list:[],
+      pno:0
+    }
+  },
+  created(){
+    this.loadMore();
+  },
+  methods:{
+    loadMore(){
+      //功能:当组件创建成功后获取第一页数据
+      // 1.创建一个url地址
+      var url="gas";
+      //1.1.将当前页码加一
+      this.pno++;
+      var obj = {pno:this.pno};
+      // 2.发送afax请求获取第一页数据
+      this.axios.get(url,{params:obj}).then(res=>{
+      // 3.将数据保存在data中
+        // console.log(res.data.data);
+        // this.list.concat() = res.data.data;
+        var rows = this.list.concat(res.data.data); //页的拼接
+        this.list = rows;
+      });
+    }
+  },
+}
+</script>
+<style scoped>
+  .goodsWrap{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+  .goods{
+    width:47%;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    margin:5px;
+    border: 1px solid #00aaef;
+    border-radius: 5px;
+    padding: 5px;
+    background: #39a8c317;
+  }
+  .goods-img{
+    width:100%;
+    border-radius: 5px;
+  }
+  .goods-info{
+    font-weight: bold;
+  }
+</style>
