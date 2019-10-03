c95b6428fad3297289a5e9622761051ef20663ec
Jason Ding <djhdder@aliyun.com>
2019-10-03 16:10:13 +0800

    add reg page

--- src/views/Reg.vue
+++ src/views/Reg.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>
-    <h1>reg</h1>
+  <div class="regWrap">
+    <h4 class="regTitle">
+      <img src="../assets/logo_big.png" />
+      因呼吸而存在
+    </h4>
+    <input
+      class="inputStyle"
+      placeholder="您的用户名是..."
+    >
+    <input
+      class="inputStyle"
+      type="password"
+      placeholder="您的密码是..."
+    >
+    <input
+      class="inputStyle"
+      type="password"
+      placeholder="请确认您的密码"
+    >
   </div>
 </template>
+<style scoped>
+  .regWrap{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .regTitle{
+    font-weight: normal;
+  }
+  .inputStyle{
+    border:none;
+    outline: none;
+    border-bottom: 1px solid #00aaef;
+  }
+</style>
